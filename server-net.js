977efcea44091594e4f8ac2b46985f534af2c6e6
gossoudarev <goss@itmo.ru>
2019-09-16 04:05:38 +0300

    привет

--- server-net.js
+++ server-net.js
@@ -4,10 +4,10 @@ const prot = process.argv[2] || 'HTTP';
 const resp =
 `${prot}/1.1 200 OK
 Content-Type: text/plain; charset=utf-8
-Content-Length: 5
+Content-Length: 12
 
-Hello
+Привет
 `;
 require('net')
-  .Server(sock => sock.on('data', (d => console.log(String(d)) || sock.end(resp))))
+  .Server(sock => sock.on('data', d => console.log(String(d)) || sock.end(resp)))
   .listen(4321);
